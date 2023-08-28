2fbe876478db42fd97656fb90b23569d655a9918
Sumanth3028 <sumanthakula05@gmail.com>
2023-08-28 12:35:58 +0530

    imported frontend

--- app.js
+++ app.js
@@ -19,26 +19,21 @@ const taskRoutes = require("./routes/taskRoutes");
 const User = require("./Models/user");
 
 const Tasks = require("./Models/task");
-const corsOptions = {
-  origin: 'https://task-management-ten-iota.vercel.app/',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // Include cookies in CORS requests (if applicable)
-  optionsSuccessStatus: 204 // Set the status code for preflight requests
-};
 
-app.use(cors({corsOptions}));
+
+app.use(cors());
 
 app.use(bodyParser.json({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.use(userRoutes);
 app.use(taskRoutes);
 
-// app.use((req, res) => {
-//   console.log("req",req.url)
-//   res.sendFile((path.join(__dirname, "public/build/index.html")))
+app.use((req, res) => {
+  console.log("req",req.url)
+  res.sendFile((path.join(__dirname,"build", "index.html")))
   
-// });
+});
 
 
 Sequelize.sync()
